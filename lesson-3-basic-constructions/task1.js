3e3a352b936fa02b711245664cc51dceea61f224
Alexander <alexvnderchernetsky@gmail.com>
2018-09-08 16:43:59 +0300

    Complete task1

--- lesson-3-basic-constructions/task1.js
+++ lesson-3-basic-constructions/task1.js
@@ -1,3 +1,23 @@
 'use strict';
 
-prompt('Введите строку', 'Счастье - это самая неопределенная вещь на свете, которая идет по самой дорогой цене');
+var userString = prompt('Введите строку', 'Счастье - это самая неопределенная вещь на свете, которая идет по самой дорогой цене.');
+
+if (userString === null) {
+  alert('Вы ничего не ввели!');
+} else {
+  var userStringLowCase = userString.toLowerCase();
+  russianVowelCount(userStringLowCase);
+}
+
+function russianVowelCount(string) {
+  var userVowelNumber = 0;
+  var russianVowel = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е'];
+  for (var i = 0; i <= string.length; i++) {
+    var userLetter = string.charAt(i);
+    if (russianVowel.indexOf(userLetter) !== -1) {
+      userVowelNumber += 1;
+    }
+  }
+  console.log(userVowelNumber);
+  return userVowelNumber;
+}
