f7b1272ce07b16223990f4ad4f04bdcec2a4ac1e
Alexander <alexvnderchernetsky@gmail.com>
2018-09-14 23:48:29 +0300

    Correct task2 because of comments

--- lesson-3-basic-constructions/script2.js
+++ lesson-3-basic-constructions/script2.js
@@ -1,109 +1,58 @@
 'use strict';
 
-function userAnswerValidation(string) {
-  var prohibitedSymbols = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ' '];
-  if ((string === null) || (string === '')) {
-    return 'nothing entered';
+function userFullNameValidation(string) {
+  if (!string) {
+    return false;
   }
-  for (var i = 0; i <= string.length; i++) {
-    var userSymbol = string.charAt(i);
-    if (prohibitedSymbols.indexOf(userSymbol) !== -1) {
-      return 'validation failed';
-    }
+  if (isNaN(parseInt(string))) {
+    return true;
   }
-  return 'validation success';
+  return false;
 }
 
 var firstName = prompt('Введите вашe имя:', 'Гадя');
 
-do  {
-  if (userAnswerValidation(firstName) === 'nothing entered') {
-    do {
-      firstName = prompt('Вы ничего не ввели! Пожалуйста введите вашe имя:', 'Гадя');
-    } while (userAnswerValidation(firstName) === 'nothing entered');
-  }
-
-  if (userAnswerValidation(firstName) === 'validation failed') {
-    do {
-      firstName = prompt('Вы ввели имя неверно! Имя не может содержать цифры и пробелы! Пожалуйста введите вашe имя:', 'Гадя');
-    } while (userAnswerValidation(firstName) === 'validation failed');
-  }
-} while (userAnswerValidation(firstName) !== 'validation success');
+while ((userFullNameValidation(firstName) === false)) {
+  firstName = prompt('Вы ввели имя неверно или ничего не ввели! Пожалуйста введите вашe имя:', 'Гадя');
+}
 
 var secondName = prompt('Введите ваше отчество:', 'Петрович');
 
-do  {
-  if (userAnswerValidation(secondName) === 'nothing entered') {
-    do {
-      secondName = prompt('Вы ничего не ввели! Пожалуйста введите вашe отчество:', 'Петрович');
-    } while (userAnswerValidation(secondName) === 'nothing entered');
-  }
-
-  if (userAnswerValidation(secondName) === 'validation failed') {
-    do {
-      secondName = prompt('Вы ввели отчество неверно! Отчество не может содержать цифры и пробелы! Пожалуйста введите вашe отчество:', 'Петрович');
-    } while (userAnswerValidation(secondName) === 'validation failed');
-  }
-} while (userAnswerValidation(secondName) !== 'validation success');
+while ((userFullNameValidation(secondName) === false)) {
+  secondName = prompt('Вы ввели отчество неверно или ничего не ввели! Пожалуйста введите вашe отчество:', 'Петрович');
+}
 
 var lastName = prompt('Введите вашу фамилию:', 'Хренова');
 
-do  {
-  if (userAnswerValidation(lastName) === 'nothing entered') {
-    do {
-      lastName = prompt('Вы ничего не ввели! Пожалуйста введите вашу фамилию:', 'Хренова');
-    } while (userAnswerValidation(lastName) === 'nothing entered');
-  }
-
-  if (userAnswerValidation(lastName) === 'validation failed') {
-    do {
-      lastName = prompt('Вы ввели фамилию неверно! Фамилия не может содержать цифры и пробелы! Пожалуйста введите вашу фамилию:', 'Хренова');
-    } while (userAnswerValidation(lastName) === 'validation failed');
-  }
-} while (userAnswerValidation(lastName) !== 'validation success');
+while ((userFullNameValidation(lastName) === false)) {
+  lastName = prompt('Вы ввели фамилию неверно или ничего не ввели! Пожалуйста введите вашу фамилию:', 'Хренова');
+}
 
 var fullYears = prompt('Сколько вам лет:', '26');
 
 function userAgeValidation(age) {
-  var prohibitedSymbols = [' ', '-', '/', '#', '+'];
-  if ((age === null) || (age === '')) {
-    return 'nothing entered';
+  if (!age) {
+    return false;
   }
   if ((age <= 0) || (age > 130)) {
-    return 'user lies';
+    return false;
   }
   for (var i = 0; i <= age.length; i++) {
+    var prohibitedSymbols = [' ', '-', '/', '#', '+'];
     var userSymbol = age.charAt(i);
     if (prohibitedSymbols.indexOf(userSymbol) !== -1) {
-      return 'validation failed';
+      return false;
     }
     if (!isFinite(age) ) {
-      return 'validation failed';
+      return false;
     }
   }
-  return 'validation success';
+  return true;
 }
 
-do {
-  if (userAgeValidation(fullYears) === 'nothing entered') {
-    do {
-      fullYears = prompt('Вы ничего не ввели! Пожалуйста введите ваш возраст:', '26');
-    } while (userAgeValidation(fullYears) === 'nothing entered');
-  }
-
-  if (userAgeValidation(fullYears) === 'user lies') {
-    do {
-      fullYears = prompt('Вы хотите меня надурить!?! Пожалуйста введите ваш настоящий возраст:', '26');
-    } while (userAgeValidation(fullYears) === 'user lies');
-  }
-
-  if (userAgeValidation(fullYears) === 'validation failed') {
-    do {
-      fullYears = prompt('Вы ввели возраст неверно! Возраст должен быть цифрой без пробелов и каких-либо знаков! Пожалуйста введите ваш возраст:', '26');
-    } while (userAgeValidation(fullYears) === 'validation failed');
-  }
-} while (userAgeValidation(fullYears) !== 'validation success');
-
+while ((userAgeValidation(fullYears) === false)) {
+  fullYears = prompt('Вы ввели возраст неверно или ничего не ввели! Пожалуйста введите ваш возраст:', '26');
+}
 
 var gender = confirm('Ваш пол - мужской?');
 var pension = false;
