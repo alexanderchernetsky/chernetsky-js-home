7f26b668f09c18d0de0aa4c3c91b92664916561a
Alexander <alexvnderchernetsky@gmail.com>
2018-09-15 14:41:12 +0300

    Complete task2

--- lesson-5-closures/script2.js
+++ lesson-5-closures/script2.js
@@ -1,9 +1,24 @@
-var objt = {
+var someObj = {
   className: 'open menu'
 };
 
-function removeClass(obj, cls) {
-  
+var someObj2 = {
+  className: 'my menu menu'
+};
+
+function removeCssClass(obj, cls) {
+  for (var key in obj) {
+    var replacedStr = obj[key].replace(cls, '');
+    obj[key] = replacedStr.trim();
+    if (replacedStr.indexOf(cls) !== 1) {
+      var replacedStr = obj[key].replace(cls, '');
+      obj[key] = replacedStr.trim();
+    }
+  }
+  return obj;
 }
 
-console.log(objt, 'open');
+console.log(removeCssClass(someObj, 'blabla'));
+console.log(removeCssClass(someObj, 'open'));
+
+console.log(removeCssClass(someObj2, 'menu'));
