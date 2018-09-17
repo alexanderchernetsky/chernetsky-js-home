6b0fef5850798c1749a6e8e238a852d6d5e69821
Alexander <alexvnderchernetsky@gmail.com>
2018-09-17 15:54:27 +0300

    Rewrite function from task1 to have ability to change both width and legth

--- lesson-5-closures/script1.js
+++ lesson-5-closures/script1.js
@@ -1,19 +1,33 @@
 'use strict';
 
-function drawGrid(scale) {
-  var gridOdd = '# # # # \n';
-  var gridEven = ' # # # #\n';
+function drawGrid(length, width) {
+  var gridSharp = '#';
+  var gridSpace = ' ';
+  var gridLineBreak = '\n';
   var grid = '';
-  for (var i = 1; i <= scale; i++) {
-    if (i % 2 !== 0) {
-      grid += gridOdd;
+  for (var n = 1; n <= length; n++) {
+    if (n % 2 !== 0) {
+      for (var i = 1; i <= width; i++) {
+        if (i % 2 !== 0) {
+          grid += gridSharp;
+        } else {
+          grid += gridSpace;
+        }
+      }
+      grid += gridLineBreak;
     } else {
-      grid += gridEven;
+      for (i = 1; i <= width; i++) {
+        if (i % 2 !== 0) {
+          grid += gridSpace;
+        } else {
+          grid += gridSharp;
+        }
+      }
+      grid += gridLineBreak;
     }
   }
   console.log(grid);
 }
 
-drawGrid(1);
-drawGrid(2);
-drawGrid(8);
+drawGrid(8, 8);
+drawGrid(10, 32);
