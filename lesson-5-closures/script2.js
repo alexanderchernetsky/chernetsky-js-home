8c905ae69fc8bdc26c2eb9dfda88535f0b233e07
Alexander <alexvnderchernetsky@gmail.com>
2018-09-17 15:19:02 +0300

    Add new object for testing the results of function

--- lesson-5-closures/script2.js
+++ lesson-5-closures/script2.js
@@ -3,10 +3,14 @@ var someObj = {
 };
 
 var someObj2 = {
-  className: 'my menu menu'
+  className: 'open menu'
 };
 
 var someObj3 = {
+  className: 'my menu menu'
+};
+
+var someObj4 = {
   className: 'my menu menu menu menu'
 };
 
@@ -21,6 +25,6 @@ function removeCssClass(obj, cls) {
 }
 
 console.log(removeCssClass(someObj, 'blabla'));
-console.log(removeCssClass(someObj, 'open'));
-console.log(removeCssClass(someObj2, 'menu'));
+console.log(removeCssClass(someObj2, 'open'));
 console.log(removeCssClass(someObj3, 'menu'));
+console.log(removeCssClass(someObj4, 'menu'));
