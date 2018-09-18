74e8c2c2684c938fa3c9518972121a6eb1c51bb3
Alexander <alexvnderchernetsky@gmail.com>
2018-09-18 16:19:05 +0300

    Correct task1.test.js because of comments

--- lesson-4-code-execution/task1.test.js
+++ lesson-4-code-execution/task1.test.js
@@ -1,35 +1,28 @@
 var defineTheBestEmployee = require('./task1');
 
-var tasksCompleted = {
-  'Anna': 29,
-  'Serg': 35,
-  'Elena': 1,
-  'Anton': 99
-};
-
 describe('function defineTheBestEmployee test', () => {
   it('who completed more tasks than others', () => {
+    var tasksCompleted = {
+      'Anna': 29,
+      'Serg': 35,
+      'Elena': 1,
+      'Anton': 99
+    };
     expect(defineTheBestEmployee(tasksCompleted)).toBe('Anton');
   });
-});
 
-var tasksCompleted2 = {
-  'Gena': 0,
-  'Anna': 39,
-  'Alex': 121,
-  'John': 99
-};
-
-describe('function defineTheBestEmployee test', () => {
   it('who completed more tasks than others', () => {
+    var tasksCompleted2 = {
+      'Gena': 0,
+      'Anna': 39,
+      'Alex': 121,
+      'John': 99
+    };
     expect(defineTheBestEmployee(tasksCompleted2)).toBe('Alex');
   });
-});
 
-var tasksCompleted3 = {};
-
-describe('function defineTheBestEmployee test', () => {
-  it('who completed more tasks than others', () => {
+  it('what will happen if the object is empty', () => {
+    var tasksCompleted3 = {};
     expect(defineTheBestEmployee(tasksCompleted3)).toBe('empty object');
   });
 });
