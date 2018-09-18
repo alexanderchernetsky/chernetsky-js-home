46736e8fe02fe19033d99e306f45e0f6887e068a
Alexander <alexvnderchernetsky@gmail.com>
2018-09-18 16:27:22 +0300

    Correct task2.test.js because of comments

--- lesson-4-code-execution/task2.test.js
+++ lesson-4-code-execution/task2.test.js
@@ -1,13 +1,12 @@
 var multiplyNumeric = require('./task2');
 
-var image = {
-  width: 100,
-  height: 400,
-  title: 'Cool image'
-};
-
 describe('function multiplyNumeric test', () => {
   it('multiply numbers in object', () => {
+    var image = {
+      width: 100,
+      height: 400,
+      title: 'Cool image'
+    };
     expect(multiplyNumeric(image)).toEqual({width: 200, height: 800, title: 'Cool image'});
   });
 });
