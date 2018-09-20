9535cd8524a186020c48e18bf1a852558adbabdf
Alexander <alexvnderchernetsky@gmail.com>
2018-09-20 15:35:06 +0300

    Change script.test.js

--- lesson-6-closures/script.test.js
+++ lesson-6-closures/script.test.js
@@ -1,2 +1,8 @@
 var johnExpenses = require('./script');
 var calcAverageTip = require('./script');
+
+describe('function calcAverageTip test', () => {
+  it('sgsgs', () => {
+    var tipsArray = [10, 20, 30];
+    expect(calcAverageTip(tipsArray)).toBe(20);
+});
