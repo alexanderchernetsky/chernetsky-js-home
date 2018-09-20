8b3ffa8d7d8d4a0c5008986299c2725faa00a30a
Alexander <alexvnderchernetsky@gmail.com>
2018-09-20 14:01:40 +0300

    Complete first part of challenge

--- lesson-6-closures/script.js
+++ lesson-6-closures/script.js
@@ -3,20 +3,27 @@
 var johnExpenses = {
   bills: [124, 48, 268, 180, 42], // dollars
   calcTip: function () {
-    var tips = [];
+    this.tips = [];
+    this.paidAmounts = [];
     for (var i = 0; i < this.bills.length; i++) {
       if (this.bills[i] <= 50) {
-        tips.push(0.2 * this.bills[i]);
+        this.tips.push(0.2 * this.bills[i]);
+        this.paidAmounts.push(0.2 * this.bills[i] + this.bills[i]);
       }
       if (this.bills[i] > 50 && this.bills[i] <= 200) {
-        tips.push(0.15 * this.bills[i]);
+        this.tips.push(0.15 * this.bills[i]);
+        this.paidAmounts.push(0.15 * this.bills[i] + this.bills[i]);
       }
       if (this.bills[i] > 200) {
-        tips.push(0.1 * this.bills[i]);
+        this.tips.push(0.1 * this.bills[i]);
+        this.paidAmounts.push(0.1 * this.bills[i] + this.bills[i]);
       }
     }
-    return tips;
+    return 0;
   }
 };
 
-console.log(johnExpenses.calcTip());
+johnExpenses.calcTip();
+
+console.log(johnExpenses.tips);
+console.log(johnExpenses.paidAmounts);
