52eb1259e24480f9b55178dedfe8037bc64488d2
Alexander <alexvnderchernetsky@gmail.com>
2018-09-22 22:12:51 +0300

    Add new version of function to script2.js because of comments

--- lesson-5-closures/script2.js
+++ lesson-5-closures/script2.js
@@ -24,7 +24,24 @@ function removeCssClass(obj, cls) {
   return obj;
 }
 
+function removeCssClass2(obj, cls) {
+  var cssClasses = obj.className ? obj.className.split(' ') : [];
+  for (var i = 0; i < cssClasses.length; i++) {
+    if (cssClasses[i] === cls) {
+      cssClasses[i].splice(i, 1);
+      i--;
+    }
+  }
+  obj.className = cssClasses.join(' ');
+  return obj;
+}
+
 console.log(removeCssClass(someObj, 'blabla'));
 console.log(removeCssClass(someObj2, 'open'));
 console.log(removeCssClass(someObj3, 'menu'));
 console.log(removeCssClass(someObj4, 'menu'));
+
+console.log(removeCssClass2(someObj, 'blabla'));
+console.log(removeCssClass2(someObj2, 'open'));
+console.log(removeCssClass2(someObj3, 'menu'));
+console.log(removeCssClass2(someObj4, 'menu'));
