19b01ac5f84463e0fb51322cbd376cbb0b6a2d7a
Alexander <alexvnderchernetsky@gmail.com>
2018-09-22 21:42:51 +0300

    Correct task1 because of comments

--- lesson-5-closures/script1.js
+++ lesson-5-closures/script1.js
@@ -6,28 +6,18 @@ function drawGrid(length, width) {
   var gridLineBreak = '\n';
   var grid = '';
   for (var n = 1; n <= length; n++) {
-    if (n % 2 !== 0) {
-      for (var i = 1; i <= width; i++) {
-        if (i % 2 !== 0) {
-          grid += gridSharp;
-        } else {
-          grid += gridSpace;
-        }
+    for (var i = 1; i <= width; i++) {
+      if ((n + i) % 2 === 0) {
+        grid += gridSharp;
+      }  else {
+        grid += gridSpace;
       }
-      grid += gridLineBreak;
-    } else {
-      for (i = 1; i <= width; i++) {
-        if (i % 2 !== 0) {
-          grid += gridSpace;
-        } else {
-          grid += gridSharp;
-        }
-      }
-      grid += gridLineBreak;
     }
+    grid += gridLineBreak;
   }
   console.log(grid);
 }
 
 drawGrid(8, 8);
 drawGrid(10, 32);
+drawGrid(14, 44);
