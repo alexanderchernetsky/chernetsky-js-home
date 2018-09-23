12cf9731124ffb2f160e40e309fbe00e0400f4d3
Alexander <alexvnderchernetsky@gmail.com>
2018-09-23 16:50:28 +0300

    Complete task2

--- lesson-7-closures/script.js
+++ lesson-7-closures/script.js
@@ -9,4 +9,20 @@ var sum = function (a) {
 console.log(sum(1)(2));
 console.log(sum(5)(-1));
 
+var interviewQuestion = function (jobTitle) {
+  return function (name) {
+    var question;
+    if (jobTitle === 'designer') {
+      question = name + ' can you please explain what UX design is?';
+    } else if (jobTitle === 'teacher') {
+      question = 'What subject do you teach ' + name + '?';
+    } else {
+      question = 'Hello ' + name + ' what do you do?';
+    }
+    return question;
+  };
+};
 
+console.log(interviewQuestion('teacher')('John'));
+console.log(interviewQuestion('designer')('John'));
+console.log(interviewQuestion('plumber')('John'));
