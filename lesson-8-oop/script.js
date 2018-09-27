6ab4cb48b63fbeae3fa8fc5de2222ea281a1187e
Alexander <alexvnderchernetsky@gmail.com>
2018-09-27 19:04:20 +0300

    Complete coding challenge

--- lesson-8-oop/script.js
+++ lesson-8-oop/script.js
@@ -1,23 +1,29 @@
 'use strict';
 
-function myQuiz() {
-  function Question(someQuest) {
+function miniQuiz() {
+  function Question(someQuest, answers, correctAnswer) {
     this.quest = someQuest;
-    this.answer = [1, 2, 3, 4];
-    this.correctAnswer = 3;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
   }
 
-  var question1 = new Question('The Beatles were an English rock band formed in? 1-New York 2-Minsk 3-Liverpool 4-Paris');
-  var question2 = new Question('Who was the first heavy metal band?? 1-Metallica 2-Iron Maiden 3-Black Sabbath 4-Judas Priest');
-  var question3 = new Question('Who is the Metallica frontman? 1-Lars Ulrich 2-John Lennon 3-James Alan Hetfield 4-Elvis Presley');
+  var question1 = new Question('The Beatles were an English rock band formed in?', ['1-Paris', '2-Minsk', '3-New York', '4-Liverpool'], 4);
+  var question2 = new Question('Who was the first heavy metal band?', ['1-Metallica', '2-Iron Maiden', '3-Black Sabbath', '4-Judas Priest'], 3);
+  var question3 = new Question('Who is the Metallica frontman?', ['1-Lars Ulrich', '2-James Hetfield', '3-John Lennon', '4-Elvis Presley'], 2);
 
   var questArray = [question1, question2, question3];
 
+  function randomDiap(n, m) {
+    return Math.floor(Math.random() * (m - n + 1)) + n;
+  }
+
+  var questionNumber = randomDiap(0, (questArray.length - 1));
+
   Question.prototype.printQuestionToConsole = function () {
-    console.log(this.quest);
+    console.log('Question ' + (questionNumber + 1) + ': ' + this.quest + ' ' + this.answers);
   };
 
-  question1.printQuestionToConsole();
+  questArray[questionNumber].printQuestionToConsole();
 
   var userAnswer = prompt('Please type the number of correct answer');
 
@@ -25,12 +31,14 @@ function myQuiz() {
     userAnswer = Number(userAnswer);
     if (userAnswer === this.correctAnswer) {
       console.log('You are right!');
+    } else if (userAnswer === 0) {
+      console.log('You didn\'t answer the question!');
     } else {
       console.log('You aren\'t right!');
     }
   };
 
-  question1.checkUserAnswer();
+  questArray[questionNumber].checkUserAnswer();
 }
 
-myQuiz();
+miniQuiz();
