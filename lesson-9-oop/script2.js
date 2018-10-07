cbd47bff400234de1b08758006c3544eab4f4520
Alexander <alexvnderchernetsky@gmail.com>
2018-10-07 10:14:29 +0300

    Correct task2 because of comments

--- lesson-9-oop/script2.js
+++ lesson-9-oop/script2.js
@@ -4,10 +4,10 @@ function isPal(someString) {
   function trim(item) {
     return item.trim();
   }
-
-  var reverseStr = someString.toLowerCase().split('').map(trim).reverse().join('');
-
-  return reverseStr === someString.toLowerCase().split('').map(trim).join('');
+  var auxArr = someString.toLowerCase().split('').map(trim);
+  var processedStr = auxArr.join('');
+  var reverseStr = auxArr.reverse().join('');
+  return reverseStr === processedStr;
 }
 
 module.exports = isPal;
