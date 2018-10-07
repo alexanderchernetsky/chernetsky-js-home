9b3fbcb86cf0e62c9d6ebea66aac6b8d2b844d0f
Alexander <alexvnderchernetsky@gmail.com>
2018-10-08 00:08:07 +0300

    Complete half of form

--- lesson-11-events/script.js
+++ lesson-11-events/script.js
@@ -1,9 +1,15 @@
 'use strict';
 
 var siteFormContainer = [
+  { legendText: 'Для внесения вашего сайта в каталог, заполните форму:' },
   { label: 'Разработчики:', type: 'text', name: 'developers', id: 'dev' },
   { label: 'Название сайта:', type: 'text', name: 'website-name', id: 'siteName' },
-  { label: 'URL сайта:', type: 'text', name: 'website-url', id: 'siteURL' }
+  { label: 'URL сайта:', type: 'text', name: 'website-url', id: 'siteURL' },
+  { label: 'Дата запуска сайта:', type: 'text', name: 'website-start', id: 'start'},
+  { label: 'Посетителей в сутки:', type: 'text', name: 'website-users', id: 'users'},
+  { label: 'E-mail для связи:', type: 'email', name: 'author-email', id: 'email'},
+  { label: 'Рубрика каталога:', type: 'text', name: 'catalog-heading', id: 'heading', options: ['бытовая техника', 'здоровье', 'домашний уют'],
+    values: ['appliances', 'health', 'home'] }
 ];
 
 (function generateForm (container) {
@@ -19,11 +25,14 @@ var siteFormContainer = [
     fieldset.appendChild(br);
   }
 
-  var legend = document.createElement('legend');
-  var legendNode = document.createTextNode('Для внесения вашего сайта в каталог, заполните форму:');
-  legend.appendChild(legendNode);
-  fieldset.appendChild(legendNode);
-  makeBreak();
+  function legendCreate(legendText) {
+    var legend = document.createElement('legend');
+    var legendNode = document.createTextNode(legendText);
+    legend.appendChild(legendNode);
+    fieldset.appendChild(legend);
+  }
+
+  legendCreate(container[0].legendText);
 
   function labelCreate (name, id) {
     var label = document.createElement('label');
@@ -33,8 +42,6 @@ var siteFormContainer = [
     fieldset.appendChild(label);
   }
 
-  labelCreate(container[0].label, container[0].id);
-
   function inputCreate (name, type, id) {
     var input = document.createElement('input');
     input.type = type;
@@ -43,23 +50,43 @@ var siteFormContainer = [
     fieldset.appendChild(input);
     makeBreak();
   }
-  
-  var inp = document.createElement('input');
-  inp.type = 'text';
-  inp.name = 'developers';
-  inp.id = 'dev';
-  fieldset.appendChild(inp);
-  makeBreak();
 
-
-
-  labelCreate('Название сайта:', 'siteName');
-
-
-
-  inputCreate('website-name', 'text', 'siteName');
+  labelCreate(container[1].label, container[1].id);
+  inputCreate(container[1].name, container[1].type, container[1].id);
 
   labelCreate(container[2].label, container[2].id);
+  inputCreate(container[2].name, container[2].type, container[2].id);
+
+  labelCreate(container[3].label, container[3].id);
+  inputCreate(container[3].name, container[3].type, container[3].id);
+
+  labelCreate(container[4].label, container[4].id);
+  inputCreate(container[4].name, container[4].type, container[4].id);
+
+  labelCreate(container[5].label, container[5].id);
+  inputCreate(container[5].name, container[5].type, container[5].id);
+
+  labelCreate(container[6].label, container[6].id);
+  inputCreate(container[6].name, container[6].type, container[6].id);
+
+  labelCreate(container[7].label, container[7].id);
+
+  function selectCreate (name, id, opt, val) {
+    var select = document.createElement('select');
+
+    for ( var i = 0; i < opt.length; i++) {
+      var option = document.createElement('option');
+      option.value = val[i];
+      var optionNode = document.createTextNode(opt[i]);
+      option.appendChild(optionNode);
+      select.appendChild(option);
+    }
+
+    fieldset.appendChild(select);
+    makeBreak();
+  }
+  
+  selectCreate(container[7].name, container[7].id, container[7].options, container[7].values);
 
 
 
