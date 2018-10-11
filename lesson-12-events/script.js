4a9acdf46df7f8bef9f6ce87587f975b1123ab9a
Alexander <alexvnderchernetsky@gmail.com>
2018-10-11 11:20:58 +0300

    Write some code for the task

--- lesson-12-events/script.js
+++ lesson-12-events/script.js
@@ -1,17 +1,67 @@
 'use strict';
 
 var picturesArr = document.getElementsByTagName('img');
-console.log(picturesArr);
 
-var cartman = document.getElementById('curt');
+var picturesOffsetLeft = [];
+var picturesOffsetTop = [];
 
-for (var i of picturesArr) {
-  i.addEventListener('mousedown', myFunc, false);
+for (var n = 0; n < picturesArr.length; n++) {
+  picturesOffsetLeft.push(picturesArr[n].offsetLeft);
+  picturesOffsetTop.push(picturesArr[n].offsetTop);
 }
 
+for (var i = 0; i < picturesArr.length; i++) {
+  picturesArr[i].style.position = 'absolute';
+  picturesArr[i].style.left = picturesOffsetLeft[i] + 'px';
+  picturesArr[i].style.top = picturesOffsetTop[i] + 'px';
 
-function myFunc(EO) {
+  picturesArr[i].addEventListener('mousedown', dragStart, false);
+}
+
+var wrapper = document.getElementById('wrapper');
+wrapper.addEventListener('mouseup', dragStop, false);
+
+var dragImage = null;
+var dragShiftX;
+var dragShiftY;
+
+function dragStart(EO) {
   EO = EO || window.event;
-  console.log('ok');
+  EO.preventDefault();
 
+  console.log('mousedown ok');
+  dragImage = EO.target;
+
+  console.log(EO.pageX);
+  console.log(EO.pageY);
+  console.log(dragImage.offsetLeft);
+  console.log(dragImage.offsetTop);
+
+  dragShiftX = EO.pageX - dragImage.offsetLeft;
+  dragShiftY = EO.pageY - dragImage.offsetTop;
+
+  console.log(dragImage);
+  console.log(dragShiftX);
+  console.log(dragShiftY);
+}
+
+function dragStop(EO) {
+  EO = EO || window.event;
+  console.log('mouseup ok');
+  dragImage = null;
+  dragShiftX = null;
+  dragShiftY = null;
+
+  console.log(dragImage);
+  console.log(dragShiftX);
+  console.log(dragShiftY);
+}
+
+function Drag_Move(EO) {
+  EO = EO || window.event;
+  console.log('mousemove ok');
+  console.log(EO.pageX);
+  console.log(EO.pageY);
+  // dragImage.style.left =   'px';
+  // dragImage.style.top =  'px';
 }
