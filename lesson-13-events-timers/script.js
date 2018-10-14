bd78f12c9850c20b307053fea9abae3e10f5a963
Alexander <alexvnderchernetsky@gmail.com>
2018-10-14 21:08:04 +0300

    Complete code

--- lesson-13-events-timers/script.js
+++ lesson-13-events-timers/script.js
@@ -18,13 +18,14 @@ minuteHand.classList.add('minute-hand');
 secondHand.classList.add('second-hand');
 
 
-var MINANGLE = (90 / 3) / 180 * Math.PI;
+var MINANGLE = (90 / 3) / 180 * Math.PI; // находим угол который нужен для построения кружков с числами и переводим его в радианы
 var angle = 0;
 
-var RADIUS = clockFace.offsetWidth / 2.5;
+var RADIUS = clockFace.offsetWidth / 2.5; // это произвольное расстояние от центра круга до центра кружков с числами
 
 var clockFaceCenterX = clockFace.offsetLeft + clockFace.offsetWidth / 2;
 var clockFaceCenterY = clockFace.offsetTop + clockFace.offsetHeight / 2;
+
 
 for (var i = 12; i > 0; i--) {
   var circleEl = document.createElement('div');
@@ -32,12 +33,10 @@ for (var i = 12; i > 0; i--) {
   var number = document.createTextNode(i);
   numberEl.appendChild(number);
   circleEl.appendChild(numberEl);
-  clockFace.appendChild(circleEl);
+  body.appendChild(circleEl);
 
   circleEl.classList.add('circle');
   numberEl.classList.add('number');
-
-  console.log(angle);
 
   var circleElCenterX = clockFaceCenterX  + RADIUS * Math.sin(angle);
   var circleElCenterY = clockFaceCenterY  - RADIUS * Math.cos(angle);
@@ -45,22 +44,34 @@ for (var i = 12; i > 0; i--) {
   circleEl.style.left = Math.round(circleElCenterX - circleEl.offsetWidth / 2) + 'px';
   circleEl.style.top = Math.round(circleElCenterY - circleEl.offsetHeight / 2) + 'px';
 
-
   angle -= MINANGLE;
-
 }
 
 
-/* var RedCenterX = Red.offsetLeft + Red.offsetWidth / 2;
-      var RedCenterY = Red.offsetTop + Red.offsetHeight / 2;
-var GreenCenterX = RedCenterX + Radius * Math.sin(Angle);
-var GreenCenterY = RedCenterY - Radius * Math.cos(Angle);
-
-Green.style.left = Math.round(GreenCenterX - Green.offsetWidth / 2) + 'px';
-Green.style.top = Math.round(GreenCenterY - Green.offsetHeight / 2) + 'px';*/
+function tickTackClock() {
+  var currTime = new Date();
 
 
-/*
+  var hours = currTime.getHours();
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+  var minutes = currTime.getMinutes();
+  var seconds = currTime.getSeconds();
+
+  var secondHandAngle = (360 / 60) * seconds;
+  secondHand.style.transform = 'rotate(' + secondHandAngle + 'deg)';
+
+  var minuteHandAngle = (360 / 60) * minutes;
+  minuteHand.style.transform = 'rotate(' + minuteHandAngle + 'deg)';
+
+  var hourHandAngle = (360 / 12) * hours;
+  hourHand.style.transform = 'rotate(' + hourHandAngle + 'deg)';
+}
+
+
+setInterval(tickTackClock, 1000);
+
 // форматирует переданную дату-время в формате чч:мм:сс
 function formatTime() {
   var hours = this.getHours();
@@ -79,16 +90,24 @@ function str0L(val, len) {
 }
 
 Date.prototype.formatRus = formatTime; // описываем новый метод для класса Date
-var el = document.createElement('div');
+
+var belTimeEl = document.createElement('div');
 var bodyAr = document.getElementsByTagName('body');
-bodyAr[0].appendChild(el);
-el.classList.add('rus-format-time');
+bodyAr[0].appendChild(belTimeEl);
+belTimeEl.classList.add('rus-format-time');
+
+var currTime = new Date();
+var processedTime = currTime.formatRus();
+belTimeEl.innerHTML = processedTime;
+
+belTimeEl.style.left = clockFaceCenterX - belTimeEl.offsetWidth / 2 + 'px';
+belTimeEl.style.top = clockFace.offsetTop + clockFace.offsetHeight / 4 + 'px';
+
 
 function tickTack() {
-  var currTime = new Date();
-  var processedTime = currTime.formatRus();
-  el.innerHTML = processedTime;
+  currTime = new Date();
+  processedTime = currTime.formatRus();
+  belTimeEl.innerHTML = processedTime;
 }
 
 setInterval(tickTack, 1000);
-*/
