a32dc32894006664d4ed30368bb85a6bfb0815fe
Alexander <alexvnderchernetsky@gmail.com>
2018-10-15 10:41:54 +0300

    Make moveMechanicalWatches function independent using css classes as arguments

--- lesson-13-events-timers/script.js
+++ lesson-13-events-timers/script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var CLOCKFACESIZE = 400; // clock diameter px (use it to scale the clock)
+var MINANGLE = (90 / 3) / 180 * Math.PI; // calculate the angle that is necessary for creating circles with numbers and translate it to radians
+
 var clockFace = document.createElement('div');
 var hourHand = document.createElement('div');
 var minuteHand = document.createElement('div');
@@ -17,43 +20,39 @@ clockFace.appendChild(hourHand);
 clockFace.appendChild(minuteHand);
 clockFace.appendChild(secondHand);
 
-var clockFaceSize = 400; // clock diameter px
+clockFace.style.width = CLOCKFACESIZE + 'px';
+clockFace.style.height = CLOCKFACESIZE + 'px';
+clockFace.style.borderRadius = CLOCKFACESIZE / 2 + 'px';
 
-clockFace.style.width = clockFaceSize + 'px';
-clockFace.style.height = clockFaceSize + 'px';
-clockFace.style.borderRadius = clockFaceSize / 2 + 'px';
-
-var hourHandLength = clockFaceSize / 4; // hour hand length
+var hourHandLength = CLOCKFACESIZE / 4; // hour hand length (devide by 4 to save proportions)
 
 hourHand.style.height = hourHandLength + 'px';
 hourHand.style.width  = hourHandLength / 5 + 'px';
 hourHand.style.borderRadius = hourHandLength / 10 + 'px';
 hourHand.style.transformOrigin =  hourHandLength / 10  + 'px bottom';
-hourHand.style.left =  clockFaceSize / 2 - hourHandLength / 10 + 'px';
-hourHand.style.top =  clockFaceSize / 2 - hourHandLength + 'px';
+hourHand.style.left =  CLOCKFACESIZE / 2 - hourHandLength / 10 + 'px';
+hourHand.style.top =  CLOCKFACESIZE / 2 - hourHandLength + 'px';
 
-var minuteHandLength = clockFaceSize / 2.8; // minute hand length
+var minuteHandLength = CLOCKFACESIZE / 2.8; // minute hand length (devide by 2.8 to save proportions)
 
 minuteHand.style.height = minuteHandLength + 'px';
 minuteHand.style.width  = minuteHandLength / 16 + 'px';
 minuteHand.style.borderRadius = minuteHandLength / 32 + 'px';
 minuteHand.style.transformOrigin =  minuteHandLength / 32  + 'px bottom';
-minuteHand.style.left =  clockFaceSize / 2 - minuteHandLength / 32 + 'px';
-minuteHand.style.top =  clockFaceSize / 2 - minuteHandLength + 'px';
+minuteHand.style.left =  CLOCKFACESIZE / 2 - minuteHandLength / 32 + 'px';
+minuteHand.style.top =  CLOCKFACESIZE / 2 - minuteHandLength + 'px';
 
-var secondHandLength = clockFaceSize / 2.2; // second hand length
+var secondHandLength = CLOCKFACESIZE / 2.2; // second hand length (devide by 2.2 to save proportions)
 
 secondHand.style.height = secondHandLength + 'px';
 secondHand.style.width  = secondHandLength / 50 + 'px';
 secondHand.style.borderRadius = secondHandLength / 100 + 'px';
 secondHand.style.transformOrigin =  secondHandLength / 100  + 'px bottom';
-secondHand.style.left =  clockFaceSize / 2 - secondHandLength / 100 + 'px';
-secondHand.style.top =  clockFaceSize / 2 - secondHandLength + 'px';
+secondHand.style.left =  CLOCKFACESIZE / 2 - secondHandLength / 100 + 'px';
+secondHand.style.top =  CLOCKFACESIZE / 2 - secondHandLength + 'px';
 
-var MINANGLE = (90 / 3) / 180 * Math.PI; // находим угол который нужен для построения кружков с числами и переводим его в радианы
-var angle = 0; // это угол на который будут отклоняться прямые к центрам кружков(в первоначальный момент угол д.б. равен 0)
-
-var RADIUS = clockFace.offsetWidth / 2.5; // это расстояние от центра циферблата до центра кружков с числами
+var angle = 0; // current angle for drawing circles with numbers(initially it should be equal 0)
+var radius = clockFace.offsetWidth / 2.5; // it's the length from the center of clock face to the center of every circle with number
 
 for (var i = 12; i > 0; i--) {
   var circleEl = document.createElement('div');
@@ -64,16 +63,16 @@ for (var i = 12; i > 0; i--) {
   clockFace.appendChild(circleEl);
 
   circleEl.classList.add('circle');
-  circleEl.style.height = clockFaceSize / 8 + 'px';
-  circleEl.style.width  = clockFaceSize / 8 + 'px';
-  circleEl.style.borderRadius = clockFaceSize / 16 + 'px';
+  circleEl.style.height = CLOCKFACESIZE / 8 + 'px';
+  circleEl.style.width  = CLOCKFACESIZE / 8 + 'px';
+  circleEl.style.borderRadius = CLOCKFACESIZE / 16 + 'px';
 
   numberEl.classList.add('number');
-  numberEl.style.fontSize = clockFaceSize / 8 + 'px';
-  numberEl.style.lineHeight = clockFaceSize / 8 + 'px';
+  numberEl.style.fontSize = CLOCKFACESIZE / 8 + 'px';
+  numberEl.style.lineHeight = CLOCKFACESIZE / 8 + 'px';
 
-  var circleElCenterX = clockFace.offsetWidth / 2 + RADIUS * Math.sin(angle);
-  var circleElCenterY = clockFace.offsetHeight / 2 - RADIUS * Math.cos(angle);
+  var circleElCenterX = clockFace.offsetWidth / 2 + radius * Math.sin(angle);
+  var circleElCenterY = clockFace.offsetHeight / 2 - radius * Math.cos(angle);
 
   circleEl.style.left = Math.round(circleElCenterX - circleEl.offsetWidth / 2) + 'px';
   circleEl.style.top = Math.round(circleElCenterY - circleEl.offsetHeight / 2) + 'px';
@@ -81,8 +80,12 @@ for (var i = 12; i > 0; i--) {
   angle -= MINANGLE;
 }
 
-function tickTackClock(hourH, minuteH, ) {
+function moveMechanicalWatches(hourHandClassName, minuteHandClassName, secondHandClassName) {
   var currTime = new Date();
+
+  var hourHandArrow = document.querySelector(hourHandClassName);
+  var minuteHandArrow = document.querySelector(minuteHandClassName);
+  var secondHandArrow = document.querySelector(secondHandClassName);
 
   var hours = currTime.getHours();
   if (hours > 12) {
@@ -92,16 +95,16 @@ function tickTackClock(hourH, minuteH, ) {
   var seconds = currTime.getSeconds();
 
   var secondHandAngle = (360 / 60) * seconds;
-  secondHand.style.transform = 'rotate(' + secondHandAngle + 'deg)';
+  secondHandArrow.style.transform = 'rotate(' + secondHandAngle + 'deg)';
 
   var minuteHandAngle = (360 / 60) * minutes;
-  minuteHand.style.transform = 'rotate(' + minuteHandAngle + 'deg)';
+  minuteHandArrow.style.transform = 'rotate(' + minuteHandAngle + 'deg)';
 
   var hourHandAngle = (360 / 12) * hours;
-  hourHand.style.transform = 'rotate(' + hourHandAngle + 'deg)';
+  hourHandArrow.style.transform = 'rotate(' + hourHandAngle + 'deg)';
 }
 
-setInterval(tickTackClock, 1000);
+setInterval(moveMechanicalWatches.bind(null, '.hour-hand', '.minute-hand', '.second-hand'), 1000);
 
 // форматирует время в формате чч:мм:сс
 function formatTime() {
@@ -120,24 +123,24 @@ function str0L(val, len) {
   return strVal;
 }
 
-Date.prototype.formatRus = formatTime; // описываем новый метод для класса Date
+Date.prototype.formatRus = formatTime; // описываем новый метод formatRus для класса Date
 
-var belTimeEl = document.createElement('div');
-clockFace.appendChild(belTimeEl);
-belTimeEl.classList.add('rus-format-time');
-belTimeEl.style.fontSize = clockFaceSize / 12 + 'px';
+var digitalWatchEl = document.createElement('div');
+digitalWatchEl.classList.add('rus-format-time');
+digitalWatchEl.style.fontSize = CLOCKFACESIZE / 12 + 'px';
+clockFace.appendChild(digitalWatchEl);
 
 var currTime = new Date();
 var processedTime = currTime.formatRus();
-belTimeEl.innerHTML = processedTime;
+digitalWatchEl.innerHTML = processedTime;
 
-belTimeEl.style.left =  clockFace.offsetWidth / 2 - belTimeEl.offsetWidth / 2 + 'px';
-belTimeEl.style.top = clockFace.offsetHeight / 2 - 2 * belTimeEl.offsetHeight + 'px';
+digitalWatchEl.style.left =  clockFace.offsetWidth / 2 - digitalWatchEl.offsetWidth / 2 + 'px';
+digitalWatchEl.style.top = clockFace.offsetHeight / 2 - 2 * digitalWatchEl.offsetHeight + 'px';
 
 function showCurrentTime() {
   currTime = new Date();
   processedTime = currTime.formatRus();
-  belTimeEl.innerHTML = processedTime;
+  digitalWatchEl.innerHTML = processedTime;
 }
 
 setInterval(showCurrentTime, 1000);
