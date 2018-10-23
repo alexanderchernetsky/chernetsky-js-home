118c06adeb2d087101ecdab3c0dc65dfa8d670fc
Alexander <alexvnderchernetsky@gmail.com>
2018-10-23 11:52:31 +0300

    Correct canvas-clock script because of comments

--- lesson-14-timers-svg-canvas/canvas/script.js
+++ lesson-14-timers-svg-canvas/canvas/script.js
@@ -4,8 +4,11 @@ const baseRadius = 400; // радиус циферблата
 const circleRadius = 20; // радиус кружков с цифрами
 const numbersBaseRadius = baseRadius / 2.5; // радиус оси цифр циферблата
 const hourArrowWidth = 20; // ширина часовой стрелочки часов
+const hourArrowLength = 80; // длина часовой стрелочки часов
 const minuteArrowWidth = 10; // ширина минутной стрелочки часов
+const minuteArrowLength = 120; // длина минутной стрелочки часов
 const secondArrowWidth = 2; // ширина секундной стрелочки часов
+const secondArrowLength = 150; // длина секундной стрелочки часов
 
 const canvas = document.getElementById('canvas');
 const cont = canvas.getContext('2d');
@@ -25,7 +28,6 @@ function createClockFaceCircle() {
   cont.arc(baseRadius / 2, baseRadius / 2, baseRadius / 2, 0, Math.PI * 2);
   cont.fillStyle = 'lightgray';
   cont.fill();
-  cont.closePath();
 }
 
 // circles with numbers
@@ -44,7 +46,6 @@ function createHourCircle(circleX, circleY) {
   cont.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
   cont.fillStyle = 'green';
   cont.fill();
-  cont.closePath();
 }
 
 function createHourCircleNumber(x, y, number) {
@@ -54,51 +55,20 @@ function createHourCircleNumber(x, y, number) {
   cont.textAlign = 'center';
   cont.textBaseline = 'middle';
   cont.fillText(number, x, y);
-  cont.closePath();
 }
 
-// hour arrow
-function createHourArrow(width, degree) {
+// one function for all 3 arrows
+function createArrow(width, length, degree) {
   cont.beginPath();
   cont.lineWidth = width;
   cont.lineCap = 'round';
   cont.translate(baseRadius / 2, baseRadius / 2);
   cont.moveTo(0, 0);
   cont.rotate(degree * Math.PI / 180);
-  cont.lineTo(0, -baseRadius / 4);
+  cont.lineTo(0, -length);
   cont.strokeStyle = 'rgba(0, 0, 0, 0.8)';
   cont.setTransform(1, 0, 0, 1, 0, 0);
   cont.stroke();
-  cont.closePath();
-}
-
-// minute arrow
-function createMinuteArrow(width, degree) {
-  cont.beginPath();
-  cont.lineWidth = width;
-  cont.lineCap = 'round';
-  cont.translate(baseRadius / 2, baseRadius / 2);
-  cont.moveTo(0, 0);
-  cont.rotate(degree * Math.PI / 180);
-  cont.lineTo(0, -baseRadius / 3);
-  cont.strokeStyle = 'rgba(0, 0, 139, 0.8)';
-  cont.stroke();
-  cont.setTransform(1, 0, 0, 1, 0, 0);
-  cont.closePath();
-}
-
-// second arrow
-function createSecondArrow(width, degree) {
-  cont.beginPath();
-  cont.lineWidth = width;
-  cont.lineCap = 'round';
-  cont.translate(baseRadius / 2, baseRadius / 2);
-  cont.moveTo(0, 0);
-  cont.rotate(degree * Math.PI / 180);
-  cont.lineTo(0, -baseRadius / 2.5);
-  cont.strokeStyle = 'red';
-  cont.stroke();
-  cont.setTransform(1, 0, 0, 1, 0, 0);
   cont.closePath();
 }
 
@@ -110,7 +80,6 @@ function createDigitalWatch(rusFormatTime) {
   cont.textAlign = 'center';
   cont.textBaseline = 'middle';
   cont.fillText(rusFormatTime, baseRadius / 2, baseRadius / 2 + baseRadius / 10);
-  cont.closePath();
 }
 
 // Logic
@@ -129,9 +98,9 @@ function updateWatch(hour, minute, second) {
   const thisSecondRotate = (second / 60) * 360;
   const thisMinuteRotate = (minute) / 60 * 360;
   const thisHourRotate = (hour + minute / 60) / 12 * 360;
-  createHourArrow(hourArrowWidth, thisHourRotate);
-  createMinuteArrow(minuteArrowWidth, thisMinuteRotate);
-  createSecondArrow(secondArrowWidth, thisSecondRotate);
+  createArrow(hourArrowWidth, hourArrowLength, thisHourRotate);
+  createArrow(minuteArrowWidth, minuteArrowLength, thisMinuteRotate);
+  createArrow(secondArrowWidth, secondArrowLength, thisSecondRotate);
 }
 
 function updateDigitalWatch(hour, minute, second) {
