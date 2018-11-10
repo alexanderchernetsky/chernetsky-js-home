d4b97647dd7077a03ddbafd3c60a26192436bca2
Alexander <alexvnderchernetsky@gmail.com>
2018-11-10 13:13:19 +0300

    Create new dir and add results of previous hometask

---
+++ lesson-20-ajax-spa/hashstorage.js
@@ -0,0 +1,20 @@
+function THashStorage() {
+  var self = this,
+      pHash = {};
+
+  self.addValue = function(key, value) {
+    pHash[key] = value;
+  };
+
+  self.getValue = function(key) {
+    return pHash[key];
+  };
+
+  self.deleteValue = function(key) {
+    return delete pHash[key];
+  };
+
+  self.getKeys = function() {
+    return (Object.keys(pHash));
+  };
+}
